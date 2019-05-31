44cc96d9e93bab3ec08887dfe1c511ef87f834c5
Prashant Kushwaha <prashantk.tps@gmail.com>
2019-05-31 21:39:03 +0100

    adding more functions

--- src/components/bullet/Bullet.js
+++ src/components/bullet/Bullet.js
@@ -1,12 +1,39 @@
 import React from "react";
 import "./bullet.css"
-const Bullet=({number})=>
+class Bullet extends React.Component
  {
-    return(
-        <div className="bullet">
-            {number}
-        </div>
-    );
+     constructor(props)
+     {
+         super(props);
+         this.state={run:1, already_active:""}
+     }
+     onClick=()=>
+     {
+         this.props.onActivate(this.props.number);
+         this.props.click(this.props.number);
+         this.setState({run:2})
+     }
+     render()
+     {
+        if(this.props.number===this.props.active && this.state.run!==1)
+        {
+            document.getElementsByClassName("bullet")[this.props.number-1].style.background="#FFFFFF";
+            document.getElementsByClassName("bullet")[this.props.number-1].style.color="black";
+            document.getElementsByClassName("bullet")[this.props.number-1].classList.add("button-glow");
+        }
+        else if(this.props.number!==this.props.active && this.state.run!==1)
+        {
+            document.getElementsByClassName("bullet")[this.props.number-1].style.background="#BB5959";
+            document.getElementsByClassName("bullet")[this.props.number-1].style.color="#FFFFFF";
+            document.getElementsByClassName("bullet")[this.props.number-1].classList.remove("button-glow");
+        }
+        return(
+            <div className="bullet" onClick={this.onClick}>
+                {this.props.number}
+            </div>
+        );
+        
+     }
 
  }
  export default Bullet;
--- src/components/page3/Page3.js
+++ src/components/page3/Page3.js
@@ -2,26 +2,69 @@ import React from "react";
 import stove from "./stove.png";
 import Bullet from "./../bullet/Bullet.js";
 import "./page3.css";
-import single from "./single.svg"
-const Page3=()=>
+import single from "./single.svg";
+import double from "./double.svg";
+import triple from "./triple.svg";
+import four from "./four.svg";
+
+class Page3 extends React.Component
 {
-    return(
-        <div className="page3">
-            <img className="stove" src={stove}></img>
-            <div className="types">
-                <h1 className="type_heading">Types of Stove Available</h1>
-                <div className="type_para_container">
-                    <p className="type_para">Take you pick from the supply chain and participate in agribusiness projects that are backed up not only by Zou, but also by the best land, family heritage, innovation and overall superior expertise.</p>
+    constructor()
+    {
+        super();
+        this.state=
+                    {
+                        active_svg:single,
+                        active_bullet:1
+                    }
+    }
+    onChange=(num)=>
+    {
+        if(num===1)
+        {
+            this.setState({active_svg:single})
+        }
+        else if(num===2)
+        {
+            this.setState({active_svg:double})
+        }
+        else if(num===3)
+        {
+            this.setState({active_svg:triple})
+        }
+        else if(num===4)
+        {
+            this.setState({active_svg:four})
+        }
+    }
+    onActivate=(num)=>
+    {
+        this.setState({active_bullet:num})
+    }
+    render()
+    {
+        console.log(this.state.active_bullet)
+        return(
+            <div className="page3">
+                <img className="stove" src={stove}></img>
+                <div className="types">
+                    <h1 className="type_heading">Types of Stove Available</h1>
+                    <div className="type_para_container">
+                        <p className="type_para">Take you pick from the supply chain and participate in agribusiness projects that are backed up not only by Zou, but also by the best land, family heritage, innovation and overall superior expertise.</p>
+                    </div>
+                    <div className="bullet_container">
+                        <Bullet number={1} click={this.onChange} active={this.state.active_bullet} onActivate={this.onActivate} />
+                        <Bullet number={2} click={this.onChange} active={this.state.active_bullet} onActivate={this.onActivate} />
+                        <Bullet number={3} click={this.onChange} active={this.state.active_bullet} onActivate={this.onActivate} />
+                        <Bullet number={4} click={this.onChange} active={this.state.active_bullet} onActivate={this.onActivate} />
+                    </div>
+                    <img className="type_svg" src={this.state.active_svg}></img>
+                    <div className="navigation_type_container">
+
+                    </div>
                 </div>
-                <div className="bullet_container">
-                    <Bullet number="1" />
-                    <Bullet number="2" />
-                    <Bullet number="3" />
-                    <Bullet number="4" />
-                </div>
-                <img className="type_svg" src={single}></img>
             </div>
-        </div>
-    );
+        );
+    }
 }
 export default Page3;
